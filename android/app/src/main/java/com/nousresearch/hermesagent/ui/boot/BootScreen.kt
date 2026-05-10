1d0c543c1d011cc78ccff8ee5f0aebf3ab0fbaf6
adybag14-cyber <adybag14@gmail.com>
2026-05-11 00:24:06 +0100

    fix(android): defer heavy startup work until first use

--- android/app/src/main/java/com/nousresearch/hermesagent/ui/boot/BootScreen.kt
+++ android/app/src/main/java/com/nousresearch/hermesagent/ui/boot/BootScreen.kt
@@ -1,14 +1,20 @@
 package com.nousresearch.hermesagent.ui.boot
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.withFrameNanos
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.nousresearch.hermesagent.ui.shell.AppShellScreen
 
 @Composable
 fun BootScreen(viewModel: BootViewModel = viewModel()) {
     val uiState by viewModel.uiState.collectAsState()
+    LaunchedEffect(Unit) {
+        withFrameNanos { }
+        viewModel.refresh()
+    }
     AppShellScreen(
         bootUiState = uiState,
         onRetryHermes = viewModel::refresh,
