02bd5af4738025619b2f3ef30d9b92a76722639d
adybag14-cyber <adybag14@gmail.com>
2026-05-13 15:23:10 +0100

    Prepare Android patch release 0.13.98

--- hermes_cli/__init__.py
+++ hermes_cli/__init__.py
@@ -14,8 +14,8 @@ Provides subcommands for:
 import os
 import sys
 
-__version__ = "0.13.97"
-__release_date__ = "2026.5.12"
+__version__ = "0.13.98"
+__release_date__ = "2026.5.13"
 
 
 def _ensure_utf8():
